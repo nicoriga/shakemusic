e5fb7b6a95165f52bf550cb1c7291ace7ca40be1
nicola rigato <nicoriga@hotmail.com>
2014-04-17 10:44:21 +0200

    ripristinato il funzionamento

--- AccelerAudio/src/com/example/acceleraudio/FirstActivity.java
+++ AccelerAudio/src/com/example/acceleraudio/FirstActivity.java
@@ -3,6 +3,7 @@ package com.example.acceleraudio;
 import com.example.acceleraudio.R;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -11,11 +12,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
  
 public class FirstActivity extends Activity {
-    /** Called when the activity is first created. */
+	
 	Button firstButton, secondButton;
 	TextView localText;
-	 ListView list;
-	  String[] sessionName = {
+	ListView list;
+	String[] sessionName = {
 		  "Sessione 1",
 	      "Sessione 2",
 	      "Sessione 3",
@@ -37,7 +38,7 @@ public class FirstActivity extends Activity {
 	      "Sessione 19",
 	      "Sessione 20"
 	  } ;
-	  String[] sessionDataMod = {
+	String[] sessionDataMod = {
 			  "12/01/2014",
 		      "05/01/2014",
 		      "25/02/2014",
@@ -59,7 +60,7 @@ public class FirstActivity extends Activity {
 		      "30/03/2014",
 		      "12/03/2014"
 		  } ;
-	  Integer[] imageId = {
+	Integer[] imageId = {
 	      R.drawable.ic_launcher,
 	      R.drawable.ic_launcher,
 	      R.drawable.ic_launcher,
@@ -81,7 +82,7 @@ public class FirstActivity extends Activity {
 	      R.drawable.ic_launcher,
 	      R.drawable.ic_launcher
 	  };
-	  String[] upsamplingRate = {
+	String[] upsamplingRate = {
 			  "44000",
 		      "48000"
 		  } ;
@@ -90,14 +91,15 @@ public class FirstActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.landscape_main_layout);
+    	//Resources res = getResources();
     	
         //---------aggiunge il generatore di evento
         //firstButton.setOnClickListener(new ButtonAction((TextView) findViewById(R.id.textView1), "bottone1"));
         
         Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_upsampling, R.id.upsamplingRate, upsamplingRate); // creo un custom adapter
-        adapter.setDropDownViewResource(R.layout.spinner_upsampling); //
-
+        // creo un custom adapter
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.upsampling_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter); // applico adapter allo spinner
 	    
 	    CustomListSession adapter1 = new CustomListSession(FirstActivity.this, sessionName, sessionDataMod, imageId);
