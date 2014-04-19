639cb73181e361cefbf8a26d71694f47e02997b4
nicola rigato <nicoriga@hotmail.com>
2014-04-19 12:55:38 +0200

    lista sessioni prelevata dal database

--- AccelerAudio/src/com/acceleraudio/activity/FirstActivity.java
+++ AccelerAudio/src/com/acceleraudio/activity/FirstActivity.java
@@ -18,72 +18,9 @@ public class FirstActivity extends Activity {
 	
 	Button newSession, preferences;
 	ListView list;
-	String[] sessionName = {
-		  "Sessione 1",
-	      "Sessione 2",
-	      "Sessione 3",
-	      "Sessione 4",
-	      "Sessione 5",
-	      "Sessione 6",
-	      "Sessione 7",
-	      "Sessione 8",
-	      "Sessione 9",
-	      "Sessione 10",
-	      "Sessione 11",
-	      "Sessione 12",
-	      "Sessione 13",
-	      "Sessione 14",
-	      "Sessione 15",
-	      "Sessione 16",
-	      "Sessione 17",
-	      "Sessione 18",
-	      "Sessione 19",
-	      "Sessione 20"
-	  } ;
-	String[] sessionDataMod = {
-			  "12/01/2014",
-		      "05/01/2014",
-		      "25/02/2014",
-		      "12/01/2014",
-		      "05/01/2014",
-		      "25/02/2014",
-		      "12/01/2014",
-		      "05/01/2014",
-		      "25/02/2014",
-		      "12/01/2014",
-		      "05/01/2014",
-		      "25/02/2014",
-		      "12/01/2014",
-		      "05/01/2014",
-		      "25/02/2014",
-		      "12/01/2014",
-		      "05/01/2014",
-		      "25/02/2014",
-		      "30/03/2014",
-		      "12/03/2014"
-		  } ;
-	Integer[] imageId = {
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher,
-	      R.drawable.ic_launcher
-	  };
+	String[] sessionName;
+	String[] sessionDataMod;
+	Integer[] imageId;
 	private DbAdapter dbAdapter; 
     private Cursor cursor;
 	  
@@ -96,22 +33,35 @@ public class FirstActivity extends Activity {
     	dbAdapter = new DbAdapter(this);
     	dbAdapter.open();
          
-        /* inserisco record di prova
+        /* inserisco record di prova 
     	dbAdapter.createSession("sessione db 1", R.drawable.ic_launcher, 1, 1, 1, 48000, "25/01/2014", "14/04/2014" );
     	dbAdapter.createSession("sessione db 2", R.drawable.ic_launcher, 1, 0, 1, 44000, "08/02/2014", "14/04/2014" );
     	dbAdapter.createSession("sessione db 3", R.drawable.ic_launcher, 0, 1, 1, 44000, "14/03/2014", "14/04/2014" );
     	dbAdapter.createSession("sessione db 4", R.drawable.ic_launcher, 1, 0, 0, 48000, "05/04/2014", "14/04/2014" );
-        */ 
+    	dbAdapter.createSession("sessione db 5", R.drawable.ic_launcher, 1, 0, 0, 48000, "05/04/2014", "14/04/2014" );
+    	dbAdapter.createSession("sessione db 6", R.drawable.ic_launcher, 1, 0, 0, 48000, "05/04/2014", "14/04/2014" );
+    	dbAdapter.createSession("sessione db 7", R.drawable.ic_launcher, 1, 0, 0, 48000, "15/04/2014", "14/04/2014" );
+    	dbAdapter.createSession("sessione db 8", R.drawable.ic_launcher, 1, 0, 0, 44000, "05/04/2014", "14/04/2014" );
+    	dbAdapter.createSession("sessione db 9", R.drawable.ic_launcher, 1, 0, 0, 48000, "05/04/2014", "14/04/2014" );
+    	dbAdapter.createSession("sessione db 10", R.drawable.ic_launcher, 1, 0, 0, 48000, "05/04/2014", "14/04/2014" );
+        */
     	
         // prelevo tutti i record 
         cursor = dbAdapter.fetchAllSession();
         
-  
+        // istanzio array
+        sessionName = new String[cursor.getCount()];
+        sessionDataMod = new String[cursor.getCount()];
+        imageId = new Integer[cursor.getCount()];
+        
         cursor.moveToFirst();
         int i = 0;
 		while ( !cursor.isAfterLast() ) {
 			 sessionName[i] = cursor.getString( cursor.getColumnIndex(DbAdapter.COLUMN_NAME) );
 			 sessionDataMod[i] = cursor.getString( cursor.getColumnIndex(DbAdapter.COLUMN_DATE_CHANGE) );
+			 imageId[i] = cursor.getInt( cursor.getColumnIndex(DbAdapter.COLUMN_IMAGE));
+			 // per ripulire il db dalle sessioni
+			 //dbAdapter.deleteSession(cursor.getInt( cursor.getColumnIndex(DbAdapter.COLUMN_SESSIONID)));
 			 cursor.moveToNext();
 			 i++;
 		}
