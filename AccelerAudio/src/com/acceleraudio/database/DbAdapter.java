4a50e5a1371ef1a5653089d9c1bc1e6c5b919648
nicoriga <nicoriga@hotmail.com>
2014-06-19 16:31:26 +0200

    - completato DbAdapter

--- AccelerAudio/src/com/acceleraudio/database/DbAdapter.java
+++ AccelerAudio/src/com/acceleraudio/database/DbAdapter.java
@@ -13,7 +13,16 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
- 
+
+/**
+ * @author Nicola Rigato
+ * @author Luca Del Salvador
+ * @author Marco Tessari
+ * @author Gruppo: Malunix
+ *
+ * classe per la gestione dell'accesso al database
+ * e delle query
+ */
 public class DbAdapter {
 	@SuppressWarnings("unused")
 	private static final String LOG_TAG = DbAdapter.class.getSimpleName();
@@ -44,6 +53,8 @@ public class DbAdapter {
 	
 	
 	/**
+	 * istanzia il dbAdapter
+	 * 
 	 * @param context il context corrente
 	 */
 	public DbAdapter(Context context) 
@@ -95,7 +106,7 @@ public class DbAdapter {
         return dateFormat.format(date);
 	}
 	
-	// creazione valori nuova sessione: con immagine sottoforma di stringa
+	/*** creazione valori nuova sessione: con immagine sottoforma di stringa ***/
 	private ContentValues createContentValuesSession(String name, String image, int axis_x, int axis_y, int axis_z, int upsampling, String creation_date, String date_change, String sensor_data_x, String sensor_data_y, String sensor_data_z, int n_data_x, int n_data_y, int n_data_z ) 
 	{
 		ContentValues values = new ContentValues();
@@ -117,7 +128,7 @@ public class DbAdapter {
 	    return values;
 	}
 	
-	// usata per l'aggiornamento delle info della sessione
+	/*** usata per l'aggiornamento delle info della sessione ***/
 	private ContentValues createContentValuesSession(String name, int axis_x, int axis_y, int axis_z, int upsampling, String date_change) 
 	{
 		ContentValues values = new ContentValues();
@@ -131,21 +142,53 @@ public class DbAdapter {
 	    return values;
 	}
     
-	// inserisci nuova sessione: versione con immagine sottoforma di stringa base64
+	/**
+	 * inserisci nuova sessione: versione con immagine sottoforma di stringa base64
+	 * 
+	 * @param name nome sessione
+	 * @param image immagine in stringa base64
+	 * @param axis_x 1 se l'asse X è selezionato
+	 * @param axis_y 1 se l'asse Y è selezionato
+	 * @param axis_z 1 se l'asse Z è selezionato
+	 * @param upsampling tipo di upsampling scelto
+	 * @param sensor_data_x dati accelerometro asse X
+	 * @param sensor_data_y dati accelerometro asse Y
+	 * @param sensor_data_z dati accelerometro asse Z
+	 * @param n_data_x numero sample asse X
+	 * @param n_data_y numero sample asse Y
+	 * @param n_data_z numero sample asse Z
+	 * @return id della sessione appena creata
+	 */
 	public long createSession(String name, String image, int axis_x, int axis_y, int axis_z, int upsampling,  String sensor_data_x, String sensor_data_y, String sensor_data_z, int n_data_x, int n_data_y, int n_data_z ) 
 	{
 		ContentValues values = createContentValuesSession(name, image, axis_x, axis_y, axis_z, upsampling, getDate(), getDate(), sensor_data_x, sensor_data_y, sensor_data_z, n_data_x, n_data_y, n_data_z );
 		return database.insertOrThrow(DATABASE_TABLE_SESSION, null, values);
 	}
 		
-	// aggiorna info sessione 
+	/**
+	 * aggiorna info sessione
+	 * 
+	 * @param sessionID id sessione
+	 * @param name nome sessione
+	 * @param axis_x 1 se l'asse X è selezionato
+	 * @param axis_y 1 se l'asse Y è selezionato
+	 * @param axis_z 1 se l'asse Z è selezionato
+	 * @param upsampling tipo di upsampling scelto
+	 * @return true se aggiornata correttamente
+	 */
 	public boolean updateSession( long sessionID, String name, int axis_x, int axis_y, int axis_z, int upsampling) 
 	{
 		ContentValues updateValues = createContentValuesSession(name, axis_x, axis_y, axis_z, upsampling, getDate());
 		return database.update(DATABASE_TABLE_SESSION, updateValues, T_SESSION_SESSIONID + "=" + sessionID, null) > 0;
 	}
 	
-	// aggiorna nome sessione 
+	/**
+	 * aggiorna nome sessione
+	 * 
+	 * @param sessionID id sessione
+	 * @param name il nuovo nome della sessione
+	 * @return true se aggiornato correttamente
+	 */
 	public boolean updateSessionName( long sessionID, String name) 
 	{
 		ContentValues updateValues = new ContentValues();
@@ -153,7 +196,13 @@ public class DbAdapter {
 		return database.update(DATABASE_TABLE_SESSION, updateValues, T_SESSION_SESSIONID + "=" + sessionID, null) > 0;
 	}
 	
-	// aggiorna immagine sessione 
+	/**
+	 * aggiorna immagine sessione
+	 * 
+	 * @param sessionID id della sessione
+	 * @param image immagine in stringa codificata base64
+	 * @return true se aggiornato correttamente
+	 */
 	public boolean updateSessionImage( long sessionID, String image) 
 	{
 		ContentValues updateValues = new ContentValues();
@@ -161,26 +210,45 @@ public class DbAdapter {
 		return database.update(DATABASE_TABLE_SESSION, updateValues, T_SESSION_SESSIONID + "=" + sessionID, null) > 0;
 	}
 	
-	// elimina sessione 
+	/**
+	 * elimina sessione
+	 * 
+	 * @param Id id sessione da eliminare
+	 * @return true se eliminato correttamente
+	 */
 	public boolean deleteSession(long Id) 
 	{
 		return database.delete(DATABASE_TABLE_SESSION, T_SESSION_SESSIONID + "=" + Id, null) > 0;
 	}
 		
-	// preleva tutte le sessioni
+	/**
+	 * preleva tutte le sessioni
+	 * 
+	 * @return cursor per scorrere i dati prelevati dal database
+	 */
 	public Cursor fetchAllSession() 
 	{
 		return database.query(DATABASE_TABLE_SESSION, new String[] { T_SESSION_SESSIONID, T_SESSION_NAME, T_SESSION_IMAGE, T_SESSION_CREATION_DATE, T_SESSION_DATE_CHANGE, T_SESSION_N_DATA_X, T_SESSION_N_DATA_Y, T_SESSION_N_DATA_Z}, null, null, null, null, T_SESSION_SESSIONID + " DESC");
 	}
    
-	// preleva sessione per ID
+	/**
+	 * preleva sessione per ID
+	 * 
+	 * @param id id sessione
+	 * @return cursor per scorrere i dati prelevati dal database
+	 */
 	public Cursor fetchSessionById(long id) 
 	{
 		Cursor mCursor = database.query(true, DATABASE_TABLE_SESSION, new String[] {T_SESSION_SESSIONID, T_SESSION_NAME, T_SESSION_IMAGE, T_SESSION_AXIS_X, T_SESSION_AXIS_Y, T_SESSION_AXIS_Z, T_SESSION_UPSAMPLING, T_SESSION_CREATION_DATE, T_SESSION_DATE_CHANGE, T_SESSION_SENSOR_DATA_X, T_SESSION_SENSOR_DATA_Y, T_SESSION_SENSOR_DATA_Z, T_SESSION_N_DATA_X, T_SESSION_N_DATA_Y, T_SESSION_N_DATA_Z }, T_SESSION_SESSIONID + "=" + id, null, null, null, null, null);     
 		return mCursor;
 	}
 	
-	// preleva sessione per ID senza i dati dell'accelerometro
+	/**
+	 * preleva sessione per ID senza i dati dell'accelerometro
+	 * 
+	 * @param sessionId id sessione
+	 * @return cursor per scorrere i dati prelevati dal database
+	 */
 	public Cursor fetchSessionByIdMinimal(long sessionId) 
 	{
 		Cursor mCursor = database.query(true, DATABASE_TABLE_SESSION, new String[] {T_SESSION_SESSIONID, T_SESSION_NAME, T_SESSION_IMAGE, T_SESSION_AXIS_X, T_SESSION_AXIS_Y, T_SESSION_AXIS_Z, T_SESSION_UPSAMPLING, T_SESSION_CREATION_DATE, T_SESSION_DATE_CHANGE, T_SESSION_N_DATA_X, T_SESSION_N_DATA_Y, T_SESSION_N_DATA_Z }, T_SESSION_SESSIONID + "=" + sessionId, null, null, null, null, null);     
@@ -188,6 +256,8 @@ public class DbAdapter {
 	}
 	
 	/**
+	 * preleva il max id nel database
+	 * 
 	 * @return il massimo id presente nel database
 	 */
 	public long getMaxId() 
@@ -246,7 +316,17 @@ public class DbAdapter {
 		}	
 	}
   
-	// unisce sessioni per ID
+	/**
+	 * unisce sessioni per ID
+	 * 
+	 * @param sessionIdList lista di id delle sessioni da unire
+	 * @param name il nome della nuova sessione
+	 * @param axis_x 1 se l'asse X è selezionato
+	 * @param axis_y 1 se l'asse Y è selezionato
+	 * @param axis_z 1 se l'asse Z è selezionato
+	 * @param upsampling quantita di upsampling scelto
+	 * @return
+	 */
 	public long mergeSession(long[] sessionIdList, String name, int axis_x, int axis_y, int axis_z, int upsampling) 
 	{
 		StringBuilder sensor_data_x = new StringBuilder();
@@ -272,7 +352,8 @@ public class DbAdapter {
 			
 			cursor.close();
 		}
-			
+		
+		// inserisco la nuova sessione... unione delle precedenti
 		ContentValues values = createContentValuesSession(name, "", axis_x, axis_y, axis_z, upsampling, getDate(), getDate(), sensor_data_x.toString(), sensor_data_y.toString(), sensor_data_z.toString(), n_data_x, n_data_y, n_data_z );
 		long sessionId = database.insertOrThrow(DATABASE_TABLE_SESSION, null, values);
 		
